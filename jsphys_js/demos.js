73ae9f234e30eb6a8642cce26ce801a06c69a856
Alex Reinhart <alex@refsmmat.com>
2011-08-06 19:36:02 -0500

    Update demo to be more specific

--- jsphys_js/demos.js
+++ jsphys_js/demos.js
@@ -11,15 +11,15 @@ var threeObjects = {"steps": [{"caption": "Three stars. The top and left stars w
                              ]
                    };
 
-var headOnObjects = {"steps": [{"caption": "Three stars. The left and right stars approach the center star at high speed.",
+var headOnObjects = {"steps": [{"caption": "Three stars. The left and right stars approach the center star at high speed - 0.9 times the speed of light.",
                                "objects": [{"object": mainSequenceStar, "x": [0, 0, 0], "p": [0, 0], "m": 1, "label": "Center"},
-                                           {"object": mainSequenceStar, "x": [-30, 10, 0], "p": [0.9, 0], "m": 1, "label": "Left"},
-                                           {"object": mainSequenceStar, "x": [30, 10, 0], "p": [-0.9, 0], "m": 1, "label": "Right"}],
+                                           {"object": mainSequenceStar, "x": [-30, 10, 0], "p": [2.1, 0], "m": 1, "label": "Left"},
+                                           {"object": mainSequenceStar, "x": [30, 10, 0], "p": [-2.1, 0], "m": 1, "label": "Right"}],
                                "frame": 0},
-                              {"caption": "Let's see that again, but this time from the perspective of the far-left star. Notice how different it appears.",
+                              {"caption": "Let's see that again, but this time from the perspective of the far-left star. The rightmost star only approaches at 0.995c &mdash; not the 1.8c you'd expect.",
                                "objects": [{"object": mainSequenceStar, "x": [0, 0, 0], "p": [0, 0], "m": 1, "label": "Center"},
-                                           {"object": mainSequenceStar, "x": [-30, 10, 0], "p": [0.9, 0], "m": 1, "label": "Left"},
-                                           {"object": mainSequenceStar, "x": [30, 10, 0], "p": [-0.9, 0], "m": 1, "label": "Right"}],
+                                           {"object": mainSequenceStar, "x": [-30, 10, 0], "p": [2.1, 0], "m": 1, "label": "Left"},
+                                           {"object": mainSequenceStar, "x": [30, 10, 0], "p": [-2.1, 0], "m": 1, "label": "Right"}],
                                "frame": 1,
                                "origin": [150, 200, 0]}
                              ]
