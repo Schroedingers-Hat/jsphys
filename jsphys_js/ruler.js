3e3eb4cfd24bba74190544960ea6e941e6d77479
Schroedingers-Hat <explodymatt@gmail.com>
2011-08-11 20:05:37 +1000

    slowly fixing it

--- jsphys_js/ruler.js
+++ jsphys_js/ruler.js
@@ -11,7 +11,8 @@ function ruler(objects, offset, vertices)
     this.distances = [];
     this.intervalSq = 0;
     this.endPoints = [quat4.create([0,0,0,0]),quat4.create([0,0,0,0])];
-    
+    this.renderPoints = [quat4.create([0,0,0,0]),quat4.create([0,0,0,0])];
+    this.times = [];
     if (objects.length == 1)
     {
         this.pos[0] = objects[0].COM.X0; //NB: Reference, don't go changing it
@@ -47,39 +48,51 @@ ruler.prototype.update = function()
                 this.boostMats = [cBoostMat(this.velocities[0]), cBoostMat(this.velocities[1])];
         }
         this.distances[0] = Math.sqrt(quat4.spaceDot(this.mDispl,this.mDispl));
+        this.times[0] = this.mDispl[0];
         mat4.multiplyVec4(this.boostMats[0],this.mDispl,this.mDisplBoosted);
         this.distances[1] = Math.sqrt(quat4.spaceDot(this.mDisplBoosted,this.mDisplBoosted));
+        this.times[1]= this.mDisplBoosted[0];
         if (this.boostMats.length == 2)
         {
             mat4.multiplyVec4(this.boostMats[1],this.mDispl,this.mDisplBoosted);
             this.distances[2] = Math.sqrt(quat4.spaceDot(this.mDisplBoosted,this.mDisplBoosted));
+            this.times[2]= this.mDisplBoosted[0];
         }
         this.intervalSq = quat4.spaceTimeDot(this.mDisplBoosted,this.mDisplBoosted);
 }        
 
 ruler.prototype.draw = function(scene)
 {
-    if( 1 || (this.endPoints[0][1] < (scene.width - scene.origin[0])   &&
-         this.endPoints[0][2] < (scene.height - scene.origin[1])  &&
-         this.endPoints[0][1] > (-scene.origin[0])  &&
-         this.endPoints[0][2] > (-scene.origin[1]))  ||
-        (this.endPoints[1][1] < (scene.width - scene.origin[0])   &&
-         this.endPoints[1][2] < (scene.height - scene.origin[1])  &&
-         this.endPoints[1][1] > (-scene.origin[0])  &&
-         this.endPoints[1][2] > (-scene.origin[1])))
-    {
+        this.renderPoints = [
+            [this.endPoints[0][1] / scene.zoom + scene.origin[0],
+             this.endPoints[0][2] / scene.zoom + scene.origin[1]],
+            [this.endPoints[1][1] / scene.zoom + scene.origin[0],
+             this.endPoints[1][2] / scene.zoom + scene.origin[1]]]
+//    if( 1 || (this.renderPoints[0][0] < (scene.width  - scene.origin[0]) &&
+//         this.renderPoints[0][1] < (scene.height - scene.origin[1]) &&
+//         this.renderPoints[0][0] > (-scene.origin[0]) &&
+//         this.renderPoints[0][1] > (-scene.origin[1])) ||
+//        (this.renderPoints[0][0] < (scene.width  - scene.origin[0]) &&
+//         this.renderPoints[0][1] < (scene.height - scene.origin[1]) &&
+//         this.renderPoints[0][0] > (-scene.origin[0]) &&
+//         this.renderPoints[0][1] > (-scene.origin[1])))
+//    {
         scene.g.fillstyle = "rgba(0, 255, 0, 0.5)";
         scene.g.beginPath();
-        scene.g.moveTo(this.endPoints[0][1] / scene.zoom+ scene.origin[0],this.endPoints[0][2] / scene.zoom + scene.origin[1]);
-        scene.g.lineTo(this.endPoints[1][1] / scene.zoom + scene.origin[0],this.endPoints[1][2] / scene.zoom+ scene.origin[1]);
+        scene.g.moveTo(this.renderPoints[0][0],this.renderPoints[0][1]);
+        scene.g.lineTo(this.renderPoints[1][0],this.renderPoints[1][1]);
         scene.g.closePath();
         scene.g.stroke();
-        scene.g.fillText("Your frame Distance:" + this.distances[0] + scene,this.endPoints[0][1] + scene.origin[0],this.endPoints[0][2] - 10 + scene.origin[0]);
-        scene.g.fillText("Interval:" + Math.sqrt(this.intervalSq), this.endPoints[0][1], this.endPoints[0][2] - 20);
-        scene.g.fillText("This object Distance:" + this.distances[1], this.endPoints[0][1], this.endPoints[0][2] - 30);
-        if(this.endPoints[2])
+        scene.g.fillText("Your frame Distance:" + Math.floor(0.5 + this.distances[0]), 
+                         this.renderPoints[0][0], this.renderPoints[0][1] + 20);
+        scene.g.fillText("This object Distance: " + Math.floor(0.5 + this.distances[1]) + 
+                          "Time: " + Math.floor(0.5 + this.times[1]), 
+                         this.renderPoints[0][0], this.renderPoints[0][1] + 30);
+        if(this.renderPoints[1])
         {
-        scene.g.fillText("This object Distance:" + this.distances[2],this.endPoints[1][1],this.endPoints[1][2] - 30);
+        scene.g.fillText("This object Distance: " + Math.floor(0.5 + this.distances[2]) + 
+                          "Time: " + Math.floor(0.5 + this.times[2]), 
+                         this.renderPoints[1][0], this.renderPoints[1][1] + 10);
         }
-    }
+//    }
 }
