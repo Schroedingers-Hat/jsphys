acbebc33861afebcb683f09faacf69e879d872cf
Alex Reinhart <alex@refsmmat.com>
2011-08-27 13:11:42 -0500

    Simple demo of the constant speed of light. Still broken due to photon bug.

--- jsphys_js/demos.js
+++ jsphys_js/demos.js
@@ -50,9 +50,20 @@ var demos = [{"name": "Velocity addition",
                                             "label": "A circle",   "shape": aCircle(10,30)}
                       ],
              "options": {"showFramePos": true, "showVisualPos": true}}]},
-            {"name": "Photons",
-             "steps": [{"caption": "Photons.",
-                        "objects": [{"object": photon, "x": [0, 0, 0], "v": [1, 0, 0]},
-                                    {"object": photon, "x": [0, 0, 0], "v": [-1, 0, 0]}]}]
+            {"name": "Speed of light",
+             "steps": [{"caption": "Two photons are emitted from the center of a ruler, heading outwards.",
+                        "objects": [{"object": photon, "x": [0, 10, 0], "v": [1, 0, 0]},
+                                    {"object": photon, "x": [0, 10, 0], "v": [-1, 0, 0]},
+                                    {"object": extendedObject, "x": [0, 0, 0], "p": [0, 0, 0], "m": 1, "shape": linesPadder([[-40, 0, 0, 0], [40, 0, 0, 0]], 3)}
+                                    ],
+                        "options": {"showFramePos": true, "showVisualPos": false}},
+                       {"caption": "Now, the same scene, from the perspective of an observe travelling past at a high speed.",
+                        "objects": [{"object": photon, "x": [0, 10, 0], "v": [1, 0, 0]},
+                                    {"object": photon, "x": [0, 10, 0], "v": [-1, 0, 0]},
+                                    {"object": extendedObject, "x": [0, 0, 0], "p": [0, 0, 0], "m": 1, "shape": linesPadder([[-40, 0, 0, 0], [40, 0, 0, 0]], 3)},
+                                    {"object": extendedObject, "x": [-40, -10, 0], "p": [5, 0, 0], "m": 1, "shape": aSphere(5, 50)}],
+                        "frame": 3,
+                        "options": {"showFramePos": true, "showVisualPos": false}}
+                      ]
             }
             ];
