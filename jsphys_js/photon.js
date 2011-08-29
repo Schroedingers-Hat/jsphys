61250738b69a4bdf34a30cf683b1ce1ad195a4da
Schroedingers-Hat <explodymatt@gmail.com>
2011-08-29 16:03:59 +1000

    Removed calcPast from photon as the concept does not apply. Reversed y coordinate in photon.drawNow @capnrefsmmat does this fix what was strange?

--- jsphys_js/photon.js
+++ jsphys_js/photon.js
@@ -51,37 +51,12 @@ photon.prototype.changeFrame = function(translation, rotation) {
     quat4.add(this.X0, this.uDisplacement);
 };
 
-photon.prototype.calcPast = function() {
-    var vDotv = quat4.spaceDot(this.V, this.V) / Math.pow(this.V[3] / c, 2);
-    var xDotx = quat4.spaceDot(this.X0, this.X0);
-    var vDotx = quat4.spaceDot(this.X0, this.V) / this.V[3] * c;
-    var a = c*c - vDotv;
-    if (xDotx === 0 || vDotv === 0) {
-        this.XView = quat4.add(this.X0, [0,0,0,0], this.XView); //Kludge 'cos I can't think of a way to do it faster w/o passing by reference.
-        this.radialVPast = 0;
-        this.radialDist = Math.sqrt(xDotx);
-        this.rPast = Math.sqrt(xDotx);
-        thisViewTime = this.rPast / c;
-        return;
-    }
-
-    this.viewTime = -(vDotx - Math.sqrt(Math.pow(vDotx,2) + a * xDotx) ) / a;
-    
-    quat4.scale(this.V, this.viewTime / this.V[3] * c, this.uDisplacement);
-    quat4.subtract(this.X0, this.uDisplacement, this.XView);
-    
-    this.rPast = Math.sqrt(Math.max(quat4.spaceDot(this.XView, this.XView), 1e-10)); 
-    this.radialVPast = (quat4.spaceDot(this.XView, this.V) / 
-                        Math.max(this.rPast,1e-10) / this.V[3] * c);
-};
-
 photon.prototype.draw = function() {
     this.drawNow();
 };
 
 photon.prototype.update = function(timeStep) {
     this.updateX0(timeStep);
-    this.calcPast();
 };
 
 photon.prototype.drawXT = function() {};
@@ -90,7 +65,7 @@ photon.prototype.drawNow = function() {
     scene.g.fillStyle = "#fff";
     scene.g.beginPath();
     scene.g.arc(this.X0[0] / scene.zoom + scene.origin[0], 
-                this.X0[1] / scene.zoom + scene.origin[1],
+                -this.X0[1] / scene.zoom + scene.origin[1],
                 2, 0, twopi);
     scene.g.fill();
 };
