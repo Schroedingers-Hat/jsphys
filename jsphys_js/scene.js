Schroedingers-Hat/jsphys

commit 3abec96da5f7181dabbaf2af3f76d2834821ff8d

Made this change earlier in realTime, but must have disconnected/restarted before push worked.

jsphys_js/scene.js
@@ -52,8 +52,10 @@ function Scene() {
             requestAnimFrame(drawScene);
             return;
         }
+		
+		this.oldFrameStartTime = this.frameStartTime;
         this.frameStartTime = new Date().getTime();
-        this.timeStep = (this.frameStartTime - this.frameEndTime) * this.timeScale;
+        this.timeStep = (this.frameStartTime - this.oldFrameStartTime) * this.timeScale;
         this.clear();
 
         this.h.drawImage(this.lightConeCanvas, 0, 0);
@@ -190,6 +192,7 @@ function Scene() {
         if (typeof demo.steps[step].frame === "number") {
             this.shiftToFrameOfObject(this.carray[demo.steps[step].frame]);
         }
+		this.frameStartTime = new Date().getTime();
     };
 
     /**
@@ -265,6 +268,7 @@ function Scene() {
     this.timeStep = 5;
     this.timeScale = 0.02;
     this.t = 0;
+	
 
     this.defaults = {"showDoppler": true,
                      "showVisualPos": true,

commit 6a2ca7352b537ef5e168e3f2448822f53a45468c

Fix pause button borkage caused by 3abec96. Don't count time spent paused.

jsphys_js/scene.js
@@ -48,7 +48,7 @@ function Scene() {
     this.draw = function() {
         if (this.carray.length === 0) { return; }
         if (this.timeScale === 0) {
-            this.frameEndTime = new Date().getTime();
+            this.frameStartTime = new Date().getTime();
             requestAnimFrame(drawScene);
             return;
         }