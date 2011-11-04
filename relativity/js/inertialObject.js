cc8eb61606f3ff3ece12336715a143ebe3ecb4cd
Alex Reinhart <alex@refsmmat.com>
2011-11-03 22:44:35 -0500

    An effort to fully document inertialObject and its variables, resulting in significant cleanup as well.
    There were several unused variables, a couple of variables which served
    similar purposes, and one or two that could just be local variables.
    @Schroedingers-Hat: I'd appreciate it if you'd check that my comments are
    accurate. This is my attempt to fully understand how everything works -- by
    writing documentation for it.

--- relativity/js/inertialObject.js
+++ relativity/js/inertialObject.js
@@ -1,58 +1,73 @@
-// Timelike is component 3, c ready
-
 "use strict";
 
 /**
  * Creates a new inertialObject with the given X coordinate, momentum, and mass.
- *
- * Public member variable reference:
- * - X0 is the current 4-vector position of the object in the current reference
- *   frame.
- * - XView is the current visible position of the object -- that is, from the
- *   current reference frame, what light would be reaching us now, and where
- *   would the object appear to be?
- * - tau is the proper time of this object.
- * - V is the velocity (dX/dt?)
- * - radialVPast is the radial XView velocity past the origin, used for Doppler
- *   shifting.
  */
 function inertialObject(X, P, m, endPt) {
+    // The 4-vector position of the object in the current reference frame
     this.X0 = X;
+    
+    // The current visible position of the object -- that is, if I'm at the 
+    // origin of the current reference frame, where do objects appear to be?
+    this.XView = quat4.create();
+    
+    // Velocity (dX/dt?)
+    this.V = quat4.scale(P, 1 / m); 
+    
+    // The initial (t=0) 4-position of the object in the current reference frame
     this.initialPt = quat4.create(X);
-    this.rPast = 1;
-	this.initialTau = 0;
-    this.rFut = 1;
+
+    // 4-position of the object's ending point, used when drawing its worldline.
     // TODO: Allow given velocity to be ignored if an endPt is supplied.
     if (endPt) this.endPt = endPt;
-    this.XView = quat4.create();
+    
+    // Position and time of the intersection of the future light cone and this 
+    // object's worldline.
     this.XFut = quat4.create();
-    this.V = quat4.scale(P, 1 / m); 
+    this.tauFut = 0;
+    this.futTime = 0;
+    
+    // Time of the intersection of the past light cone and this object's worldline
+    this.tauPast = 0;
+    this.viewTime = 0;
+
     // Relativistic velocity, or momentum/mass.
     genEnergy(this.V, c, m);
-    this.displace = quat4.create([0, 0, 0, 0]);
+    
+    // A temporary variable used for computing the displacement of the object
+    // for each timeStep, or in a new reference frame
+    this.displace = quat4.create();
+    
+    // Proper time of this object.
+    this.initialTau = 0;
     this.tau = 0;
-    this.tauPast = 0;
-    this.tauFut = 0;
-    this.uDisplacement = quat4.create();
-    this.viewTime = 0;
-    this.futTime = 0;
+    
+    // The radial XView velocity past the origin, used for Doppler shifting
     this.radialVPast = 0;
     this.radialVFut = 0;
 }
 
 inertialObject.prototype.updateX0 = function(timeStep) {
+    // Compute the distance we must move the object to bring it to "now".
+    // V[3] is essentially gamma, so we dilate the timeStep by gamma and
+    // multiply V by the result, giving us the total distance to travel.
     quat4.scale(this.V, timeStep / this.V[3], this.displace);
-    //Increase proper time.
+    
+    // Increase our proper time.
     this.tau += timeStep / this.V[3] * c;
-    //Bring it to now.
+    
+    // Bring it to now.
     quat4.add(this.X0, this.displace);
-    this.X0[3] = this.X0[3] - timeStep;
+    
+    // Shift our time coordinate ahead by timeStep.
+    this.X0[3] -= timeStep;
     if (this.initialPt) this.initialPt[3] -= timeStep;
     if (this.endPt) this.endPt[3] -= timeStep;
 };
 
-inertialObject.prototype.changeFrame = function(translation1, rotation, translation2)
-{
+// Shift the reference frame by translation1, boost by rotation, then shift again
+// with translation2
+inertialObject.prototype.changeFrame = function(translation1, rotation, translation2) {
     // Translate.
     quat4.subtract(this.X0, translation1);
     quat4.subtract(this.initialPt, translation1);
@@ -68,20 +83,20 @@ inertialObject.prototype.changeFrame = function(translation1, rotation, translat
 
     // Point is now at wrong time.
     // Find displacement to current time.
-    quat4.scale(this.V, -this.X0[3] / this.V[3], this.uDisplacement);
+    quat4.scale(this.V, -this.X0[3] / this.V[3], this.displace);
     
     // Bring to current time.
-    quat4.add(this.X0, this.uDisplacement);
-    this.tau += this.uDisplacement[3] / this.V[3] * c;
+    quat4.add(this.X0, this.displace);
+    this.tau += this.displace[3] / this.V[3] * c;
     if (translation2) {
         quat4.subtract(this.X0, translation2);
         // Wrong time again.
         // Find displacement to current time.
-        quat4.scale(this.V, -this.X0[3] / this.V[3], this.uDisplacement);
+        quat4.scale(this.V, -this.X0[3] / this.V[3], this.displace);
     
         // Bring to current time.
-        quat4.add(this.X0, this.uDisplacement);
-        this.tau += this.uDisplacement[3] / this.V[3] * c;
+        quat4.add(this.X0, this.displace);
+        this.tau += this.displace[3] / this.V[3] * c;
 
         if (this.initialPt) quat4.subtract(this.initialPt, translation2);
         if (this.endPt) quat4.subtract(this.endPt, translation2);
@@ -110,18 +125,18 @@ inertialObject.prototype.calcPast = function() {
     timeSqrt = Math.sqrt(Math.pow(vDotx,2) + a * xDotx) ;
 
     this.viewTime = -(vDotx - timeSqrt) / a * c ;
-    quat4.scale(this.V, this.viewTime / this.V[3], this.uDisplacement);
-    quat4.subtract(this.X0, this.uDisplacement, this.XView);
+    quat4.scale(this.V, this.viewTime / this.V[3], this.displace);
+    quat4.subtract(this.X0, this.displace, this.XView);
     
-    this.rPast = Math.sqrt(Math.max(quat4.spaceDot(this.XView, this.XView), 1e-10)); 
+    var rPast = Math.sqrt(Math.max(quat4.spaceDot(this.XView, this.XView), 1e-10)); 
     this.radialVPast = (quat4.spaceDot(this.XView, this.V) / 
-                        Math.max(this.rPast,1e-10) / this.V[3] * c);
-    this.tauPast = this.tau - this.uDisplacement[3]/this.V[3] * c;
+                        Math.max(rPast, 1e-10) / this.V[3] * c);
+    this.tauPast = this.tau - this.displace[3]/this.V[3] * c;
 
-    this.futTime = -(vDotx + timeSqrt ) / a * c;
+    this.futTime = -(vDotx + timeSqrt) / a * c;
     
-    quat4.scale(this.V, this.futTime / this.V[3], this.uDisplacement);
-    quat4.subtract(this.X0, this.uDisplacement, this.XFut);
+    quat4.scale(this.V, this.futTime / this.V[3], this.displace);
+    quat4.subtract(this.X0, this.displace, this.XFut);
     
-    this.tauFut = this.tau - this.uDisplacement[3] / this.V[3] * c;
+    this.tauFut = this.tau - this.displace[3] / this.V[3] * c;
 };
